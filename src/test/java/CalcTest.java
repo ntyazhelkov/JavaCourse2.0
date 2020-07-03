2882e3979b482ef711388dde32498a9a9c069ed4
ntyazhelkov <pronger95@gmail.com>
2020-07-03 19:49:03 +0300

    Задание № 8.1 - Добавил тесты на проверку исключений

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,28 +1,35 @@
 import org.junit.Assert;
 import org.junit.Test;
+import task5.operations.IncorrectOperationTypeExeption;
 import task5.operations.Operations;
+
+import java.util.InputMismatchException;
 
 
 public class CalcTest {
 
     @Test
     public void testSumOperations () throws Exception {
-        Operations operations = new Operations();
-        Assert.assertEquals(30, operations.calc(14, 16, "+"), 30);
+        Assert.assertEquals(30, Operations.calc(14, 16, "+"), 30);
     }
     @Test
     public void testMinOperations () throws Exception {
-        Operations operations = new Operations();
-        Assert.assertEquals(30, operations.calc(100, 70, "-"), 30);
+        Assert.assertEquals(30, Operations.calc(100, 70, "-"), 30);
     }
     @Test
     public void testMultOperations () throws Exception {
-        Operations operations = new Operations();
-        Assert.assertEquals(30, operations.calc(6, 5, "*"), 30);
+        Assert.assertEquals(30, Operations.calc(6, 5, "*"), 30);
     }
     @Test
     public void testDivOperations () throws Exception {
-        Operations operations = new Operations();
-        Assert.assertEquals(30, operations.calc(900, 30, "/"), 30);
+        Assert.assertEquals(30, Operations.calc(900, 30, "/"), 30);
+    }
+    @Test(expected = IncorrectOperationTypeExeption.class)
+    public void testIncorrectOperationTypeExeption() throws Exception{
+        Operations.calc(4,5,"d");
+    }
+    @Test(expected = ArithmeticException.class)
+    public void testArithmeticException() throws Exception{
+        Operations.calc(4,0,"/");
     }
 }
